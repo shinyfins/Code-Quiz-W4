79983634192b3c52f833eda4aad0f3a1b14d6c71
Your Name <mcpgco@gmail.com>
2022-02-25 22:17:55 -0700

    Decided on questions and set up evenlistener sorta kinda

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+start.addEventListener("click", goGame );
+
+function goGame() {
+
+}
+
+/*
+1 When are curly brakcets used?
+
+for function for eventlisteners for logs for eating pudding
+
+2 What is used to put java script into html?
+
+add script? add js? add java? add a pinch of salt?
+
+3 Where do you put the javascript in the html?
+
+in the body? the head? the footer? the header?
+
+4 Does java have a boiler plate?
+
+Absolutely! it does. 100%. NO.
+
+5 What symbol is used to identify classes in java?
+# or . or @ or ^
+
+6 How do you call a function in java?
+
+funtction with parantheses? function with !? function with %? function with a bullseye?
