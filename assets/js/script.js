1e54950c18aa59153f29bbc3d3c1a7a5a26909b8
Your Name <mcpgco@gmail.com>
2022-02-26 13:24:54 -0700

    added variables holdign all our Q's and A's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+var questions = [
+    {
+        title: "When are curly brakcets used?",
+        choices: ["what is a function", "what is an eventListener?", "what is a log?", "what is more delicious then pudding"],
+        answer: "what is a function"
+    },
+    {
+        title: "What is used to put java script into html?",
+        choices: ["what is a script?", "What is js?", "what is java?", "what is a pinch of salt?"],
+        answer: "what is a script?"
+    },
+    {
+        title: "Where do you put the javascript in the html?",
+        choices: ["What is the body?", "What is the chrome dome?", "What is the footer?", "What is the header?"],
+        answer: "What is the body?"
+    },
+    {
+        title: "Does java have a boiler plate?",
+        choices: ["What is absolutely!", "What is for certain!", "What is 100%.", "What is NO."],
+        answer: "What is NO."
+    },
+    {
+        title: "What symbol is used to identify classes in java?",
+        choices: ["What is #?", "What is a period.", "what is @?", "What is ^?"],
+        answer: "What is a #?"
+    },
+    {
+        title: "How do you call a function in  java?",
+        choices: ["What are parentheses?", "What is a ! ?", "What is a % ?", "What is a bullseye 🎯?"], 
+        answer: "What are parantheses?"
+    },
+
+];
+
+
+
+
+
 start.addEventListener("click", goGame );
 
 function goGame() {
@@ -5,25 +43,14 @@ function goGame() {
 }
 
 /*
-1 When are curly brakcets used?
-
-for function for eventlisteners for logs for eating pudding
-
-2 What is used to put java script into html?
-
-add script? add js? add java? add a pinch of salt?
-
-3 Where do you put the javascript in the html?
-
-in the body? the head? the footer? the header?
-
-4 Does java have a boiler plate?
-
-Absolutely! it does. 100%. NO.
-
-5 What symbol is used to identify classes in java?
-# or . or @ or ^
-
 6 How do you call a function in java?
 
 funtction with parantheses? function with !? function with %? function with a bullseye?
+
+
+
+GOALS FOR WORK 
+
+When i press start a question with multi button options appears
+when i press a button it goes to the next question
+once all q's are over the game ends
